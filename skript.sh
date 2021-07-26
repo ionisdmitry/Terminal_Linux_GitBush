c9010eeb7764114d250a553ce81f7c3247f3648b
Ionis Dmitry <85285550+ionisdmitry@users.noreply.github.com>
2021-07-26 13:04:45 +0300

    Add files via upload

---
+++ skript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /d/Курсы/для_скрипта/
+pwd 
+mkdir folder1 folder2 folder3 
+cd folder1
+touch file1.txt file2.txt file3.txt file4.json file5.json
+mkdir 1folder1 2folder2 3folder3
+ls -la
+mv file1.txt /d/Курсы/для_скрипта/folder1/1folder1
+mv file2.txt /d/Курсы/для_скрипта/folder1/1folder1
+
